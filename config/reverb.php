e33c6b3e91f82a9a564cd11b68d0962d048f0d52
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 14:48:37 +0300

    Configure TLS settings for Reverb server

--- config/reverb.php
+++ config/reverb.php
@@ -34,7 +34,11 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert" => 'etc/nginx/ssl/server.crt',
+                    'local_pk" => 'etc/nginx/ssl/server.key',
+                    'verify_peer' => false
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
