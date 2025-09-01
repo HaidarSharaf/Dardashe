e1853e8d6e5b15b82443ebbe8e313c2085612ef3
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 14:08:56 +0300

    Change wssPort to 443 in Echo configuration

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,7 @@ window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wssPort: import.meta.env.VITE_REVERB_PORT,
+  wssPort: 443,
   forceTLS: true,
   enabledTransports: ['wss'],
 });
