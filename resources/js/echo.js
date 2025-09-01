995c893a567a7529a26564b5035e36805cb928c5
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 12:38:10 +0300

    Update echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,9 +6,9 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: ws.dardashe.site,
+    wsPort: 8080,
+    wssPort: 8080,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
