bab6dcf182582c4e3b46f2a8993ecedbbcd7a7ff
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 13:46:16 +0300

    Refactor Echo configuration for WebSocket transport

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,12 +4,10 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: true,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wssPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: true,
+  enabledTransports: ['wss'],
 });
