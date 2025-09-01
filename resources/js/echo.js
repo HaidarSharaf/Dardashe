67e7bd71fc485f1a3b137832db77749c3eb5cb64
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 14:50:05 +0300

    Change WebSocket ports and enabled transports

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,8 @@ window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wssPort: 443,
+  wsPort: 6001,
+  wssPort: 6001,
   forceTLS: true,
-  enabledTransports: ['wss'],
+  enabledTransports: ['ws', 'wss'],
 });
