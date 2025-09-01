f817d26bd428eae5f9180c51ccf2875f3d198531
Haidar Sharaf <haidarsharaf6@gmail.com>
2025-09-01 15:10:47 +0300

    Change wsPort to wsPath in Echo configuration
    Updated WebSocket configuration for Echo.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,7 @@ window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: 6001,
+  wsPath: '/app',
   wssPort: 6001,
   forceTLS: true,
   enabledTransports: ['ws', 'wss'],
